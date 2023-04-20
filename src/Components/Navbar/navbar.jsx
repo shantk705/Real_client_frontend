e1f0629845ad1ab434fcda1a3ce0776e396501e6
raoul-baddawi <raoulbaddawi1@gmail.com>
2023-04-20 23:27:29 +0300

    finished login

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import logo from '../../Assets/logo.png';
 
-function Navbar() {
+function Navbar({ onButtonClick }) {
   const [show, setShow] = useState(false);
   const [nav, setNav] = useState(false)
   const [menu, setMenu] = useState("nav-links");
@@ -57,7 +57,7 @@ function Navbar() {
           </a>
         </li>
         <li className={nav ? 'maintain' : 'normal'}>
-          <a href="/about" className={location.pathname === '/about' ? 'active' : ''}>
+          <a href="/aboutus" className={location.pathname === '/aboutus' ? 'active' : ''}>
             About us
           </a>
         </li>
@@ -73,9 +73,9 @@ function Navbar() {
         </li>
       </ul>
       <div className={nav ? 'head-icons' : 'header-icons'}>
-        <a href="/" className="user">
+        <p onClick={onButtonClick} className="user">
           <i className="ri-user-fill"></i>Sign-in
-        </a>
+        </p>
         <div className={icon} id="menu-icon" onClick={toggle}></div>
       </div>
     </header>
