dc85ee477445838aa764358d381011ebf482088c
hasanelmestrah <hasanelmestrah@gmail.com>
2023-04-26 00:50:27 +0300

    add new div,css changes

--- src/Pages/Contactus/contact.jsx
+++ src/Pages/Contactus/contact.jsx
@@ -1,8 +1,7 @@
-import './contact.css'
+import "./contact.css";
 import axios from "axios";
 import React, { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
-
 
 const ContactUs = () => {
   const form = useRef();
@@ -64,43 +63,47 @@ const ContactUs = () => {
 
   return (
     <>
-      <div className="contact-us">
-          <h1>Contact Us</h1>
-        <form className="contact-form" ref={form} onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="fullName"
-            value={fullName}
-            placeholder="Enter your Full name"
-            onChange={onChange}
-            required
-          />
+      <div className="divplus">
+        <div className="contact-us">
+          <h1>CONTACT US</h1>
+          <form className="contact-form" ref={form} onSubmit={onSubmit}>
+            <input
+              type="text"
+              name="fullName"
+              value={fullName}
+              placeholder="Enter your Full name"
+              onChange={onChange}
+              required
+            />
 
-          <input
-            type="text"
-            name="mail"
-            value={mail}
-            placeholder="Enter your email "
-            onChange={onChange}
-            required
-          />
-          {error === "Email is required" && (
-            <div style={{ color: "red" }}>{error}</div>
-          )}
-          <input
-            type="text"
-            name="Message"
-            value={Message}
-            placeholder="Enter your Message"
-            onChange={onChange}
-            required
-          />
+            <input
+              type="text"
+              name="mail"
+              value={mail}
+              placeholder="Enter your email "
+              onChange={onChange}
+              required
+            />
+            {error === "Email is required" && (
+              <div style={{ color: "red" }}>{error}</div>
+            )}
+            <input
+              className="message"
+              type="text"
+              name="Message"
+              value={Message}
+              placeholder="Enter your Message"
+              onChange={onChange}
+              required
+            />
             <button className="form-sbmt" type="submit" onClick={onSubmit}>
               submit
             </button>
-        </form>
+          </form>
+        </div>
       </div>
-    </>);
+    </>
+  );
 };
 
 export default ContactUs;
