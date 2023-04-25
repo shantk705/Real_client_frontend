c9da5100e0323f4fa116e5c925cd396ce1c0fb2c
shant-k <shantk650@gmail.com>
2023-04-25 09:28:52 +0300

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { Route, Routes, BrowserRouter} from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
-import './App.css';
-import ContactUs from './Pages/Contactus/contact';
+import "./App.css";
+import ContactUs from "./Pages/Contactus/contact";
 
-import Home from './Pages/Home/home';
-import React, { useState } from 'react';
+import Home from "./Pages/Home/home";
+import React, { useState } from "react";
 
-import Layout from "./Pages/layout.jsx"
-import Category from './Pages/Shop/category';
-import Shop from './Pages/Shop/shop';
-import Discounts from './Pages/Discounts/discount';
-import AboutUs from './Pages/Aboutus/AboutUs'
-import LoginUp from './Components/popAuth/LoginUp';
-import SideBar from "./Components/SideBar"
+import Layout from "./Pages/layout.jsx";
+import Category from "./Pages/Shop/category";
+import Shop from "./Pages/Shop/shop";
+import Discounts from "./Pages/Discounts/discount";
+import AboutUs from "./Pages/Aboutus/AboutUs";
+import LoginUp from "./Components/popAuth/LoginUp";
+import SideBar from "./Components/SideBar";
 
 function App() {
   const [showPopUp, setShowPopUp] = useState(false);
@@ -27,36 +27,32 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
-   <Routes>
-   <Route element={<SideBar/>}>
-   <Route path="/admin"/>
-   </Route>
-   </Routes>
-   </BrowserRouter>
-      
-     
-        <main className={showPopUp ? "none" : "main" }>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<SideBar />}>
+            <Route path="/admin" />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      <main className={showPopUp ? "none" : "main"}>
         <BrowserRouter>
           <Routes>
-         
-         <Route element={<Layout/>}>
-              <Route path='/' element={<Home />} />
-              <Route path='/shop' element={<Category/>} />
-              <Route path='/shop/:category_id' element={<Shop />} />
-              <Route path='/contactus' element={<ContactUs />} />
-              <Route path='/discounts' element={<Discounts />} />
-              <Route path='/aboutUs' element={<AboutUs />} />
-
-              </Route>
-              </Routes>
-         
-          </BrowserRouter>
-          <LoginUp showPopUp={showPopUp} onCloseButtonClick={handleCloseButtonClick}/>
-        </main>
-        
-        
-     
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/shop" element={<Category />} />
+              <Route path="/shop/:category_id" element={<Shop />} />
+              <Route path="/contactus" element={<ContactUs />} />
+              <Route path="/discounts" element={<Discounts />} />
+              <Route path="/aboutUs" element={<AboutUs />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <LoginUp
+          showPopUp={showPopUp}
+          onCloseButtonClick={handleCloseButtonClick}
+        />
+      </main>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {colors:{
+      "light-orange": "rgb(252, 185, 0)"
+    }},
   },
   plugins: [],
 }
